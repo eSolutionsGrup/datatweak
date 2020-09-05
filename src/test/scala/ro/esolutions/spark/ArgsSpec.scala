252aa4fb88b96a4ec2003bd3a3f2642f3fb186ee
Lucian Neghina <lucian.neghina@esolutions.ro>
2020-09-05 11:13:00 +0300

    rename AppArgs

---
+++ src/test/scala/ro/esolutions/spark/ArgsSpec.scala
@@ -0,0 +1,21 @@
+package ro.esolutions.spark
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class ArgsSpec extends FlatSpec with Matchers {
+
+  it should "fails without arguments" in  {
+    val args = Args(Array[String]())
+    args shouldBe None
+  }
+
+  it should "loads the jobname" in {
+    val args = Args(Array("-j", "JobName"))
+    args.get.appName shouldBe "JobName"
+  }
+
+  it should "ignore UnknownArgument" in {
+    val args = Args(Array("-j", "JobName", "unknownArgument"))
+    args.get.appName shouldBe "JobName"
+  }
+}
